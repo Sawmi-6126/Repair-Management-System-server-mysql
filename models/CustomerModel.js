eb70dacb59bd646f1cb296ae046ab1ded45b45ac
Sawmi-6126 <sawmi.sutha99@gmail.com>
2022-12-19 23:07:38 +0000

    customer

---
+++ models/CustomerModel.js
@@ -0,0 +1,23 @@
+import Sequelize from "sequelize";
+import db from "../config/db.js";
+import { DataTypes } from "sequelize";
+
+const Customer = db.define(
+  "customer",
+  {
+    f_name: DataTypes.STRING,
+    l_name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    email: DataTypes.STRING,
+    mobile: DataTypes.INTEGER,
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default Customer;
+
+(async () => {
+  await db.sync();
+})();
---
+++ routes/CustomerRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getCustomer } from "../controllers/CustomerController.js";
+
+const router = express.Router();
+
+router.get("/customer", getCustomer);
+
+export default router;
